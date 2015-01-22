6a88f8791eec6aa73f20a67a06404112b02bfb89
SxDean <subzero.cc@live.nl>
2015-01-22 22:57:09 +0100

    push

--- src/main/java/com/test/app/App.java
+++ src/main/java/com/test/app/App.java
@@ -1,12 +1,11 @@
 package com.test.app;
-
-import java.util.Scanner;
+import java.util.Scanner; //imports the scanner function
 public class App 
 {
     public static void main( String[] args )
 	{
-	    System.out.println("Voer jouw hele naam in:");
-	    Scanner voornaam = new Scanner(System.in);
-	    System.out.println("Welkom, " + voornaam.nextLine() + ", dit is mijn Maven applicatie.");
+	    System.out.println("Voer jouw hele naam in:"); //imput
+	    Scanner voornaam = new Scanner(System.in); //variable voornaam gets the input
+	    System.out.println("Welkom, " + voornaam.nextLine() + ", dit is mijn Maven applicatie."); //print the variable and the text
 	}
 }
